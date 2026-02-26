633a9389cfdd125a4dd57d79ab504a890d732eda
Krati Bhatia <krati.techie@gmail.com>
2026-02-26 22:42:28 +0530

    DSA Leetcode question no 1929 with 2 loops

---
+++ arrays/concatente.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        result = []
+
+        for num in nums:
+            result.append(num)
+
+        for num in nums:
+            result.append(num)
+
+        return result
+        
+
+        

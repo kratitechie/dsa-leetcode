c5a81cf04e5377a329a9a5b662f7a50c0d8bfd06
Krati Bhatia <krati.techie@gmail.com>
2026-03-18 22:39:42 +0530

    Adding LC 739 Daily Temperature

---
+++ Stack/739_daily_temperature.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        
+        stack = []  # store indices
+        result = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            
+            stack.append(i)
+        
+        return result

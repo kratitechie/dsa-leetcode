ad26a5cf58ca09a14dfe1c5cc16748d531702d91
Krati Bhatia <krati.techie@gmail.com>
2026-03-30 23:40:37 +0530

    Adding DSA LC 567, permutation in strings

--- Stack/739_daily_temperature.py
+++ Stack/739_daily_temperature.py
@@ -1,16 +1,22 @@
 class Solution:
     def dailyTemperatures(self, temperatures):
-        
-        stack = []  # store indices
+        stack = []
         result = [0] * len(temperatures)
 
         for i in range(len(temperatures)):
-            
             while stack and temperatures[i] > temperatures[stack[-1]]:
-                
                 prev_index = stack.pop()
                 result[prev_index] = i - prev_index
-            
+
             stack.append(i)
-        
+
         return result
+
+
+# 👇 THIS PART RUNS YOUR FUNCTION
+temps = [73, 74, 75, 71, 69, 72, 76, 73]
+
+sol = Solution()
+answer = sol.dailyTemperatures(temps)
+
+print(answer)
